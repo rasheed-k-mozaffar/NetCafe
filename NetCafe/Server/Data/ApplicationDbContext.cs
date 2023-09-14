aa24a5c417ae8e1dd30e314e53c7107f4e1c4cc0
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-09-14 20:18:49 +0300

    Created the db context

---
+++ NetCafe/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using NetCafe.Server.Data.Configuration;
+
+namespace NetCafe.Server.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Series> Series { get; set; }
+    public DbSet<NewsLetterSub> NewsLetterSubs { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
+        modelBuilder.ApplyConfiguration(new TagConfiguration());
+        modelBuilder.ApplyConfiguration(new CommentConfiguration());
+        modelBuilder.ApplyConfiguration(new SeriesConfiguration());
+        modelBuilder.ApplyConfiguration(new NewsLetterSubConfiguration());
+    }
+}
