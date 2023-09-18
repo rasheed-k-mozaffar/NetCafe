c507cdae909c539f0ba35f896c068c045f951883
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-09-18 10:24:25 +0300

    Seeded the roles and main data to the database

--- NetCafe/Server/Data/ApplicationDbContext.cs
+++ NetCafe/Server/Data/ApplicationDbContext.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using NetCafe.Server.Data.Configuration;
 
 namespace NetCafe.Server.Data;
 
 public class ApplicationDbContext : IdentityDbContext<AppUser>
 {
+    private readonly IConfiguration configuration;
     public DbSet<Post> Posts { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Series> Series { get; set; }
     public DbSet<NewsLetterSub> NewsLetterSubs { get; set; }
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    { }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+    {
+        this.configuration = configuration;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -24,5 +28,37 @@ public class ApplicationDbContext : IdentityDbContext<AppUser>
         modelBuilder.ApplyConfiguration(new CommentConfiguration());
         modelBuilder.ApplyConfiguration(new SeriesConfiguration());
         modelBuilder.ApplyConfiguration(new NewsLetterSubConfiguration());
+
+        // seeding roles to the database
+        modelBuilder.Entity<IdentityRole>().HasData
+        (
+            new IdentityRole { Id = configuration["Roles:AuthorRID"]!, Name = "AUTHOR", NormalizedName = "AUTHOR" },
+            new IdentityRole { Id = configuration["Roles:UserRID"]!, Name = "USER", NormalizedName = "USER" }
+        );
+
+        // this hasher instance will be used to hash the author's password
+        var hasher = new PasswordHasher<AppUser>();
+        modelBuilder.Entity<AppUser>().HasData
+        (
+          new AppUser
+          {
+              Id = configuration["Author:ID"]!,
+              FullName = configuration["Author:FullName"],
+              Email = configuration["Author:Email"],
+              NormalizedEmail = configuration["Author:Email"]!.ToUpper(),
+              PasswordHash = hasher.HashPassword(null!, $"{configuration["Author:Password"]}"),
+              SecurityStamp = null,
+          }
+        );
+
+        // seeding the relation and assignment of the author role to the main account
+        modelBuilder.Entity<IdentityUserRole<string>>().HasData
+        (
+          new Microsoft.AspNetCore.Identity.IdentityUserRole<string>
+          {
+              RoleId = configuration["Roles:AuthorRID"]!,
+              UserId = configuration["Author:ID"]!
+          }
+        );
     }
 }
