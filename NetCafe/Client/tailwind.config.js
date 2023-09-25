fd76d971cdc6de6b38eb51c842114533f074e914
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-09-25 15:25:06 +0300

    Added tailwind css to the project

---
+++ NetCafe/Client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./**/*.{razor,html}'],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
