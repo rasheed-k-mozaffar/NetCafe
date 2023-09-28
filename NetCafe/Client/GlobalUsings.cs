c578120dbaa790928dfdc6ca60794add3c0b47b9
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-09-28 09:22:35 +0300

    Changes

--- NetCafe/Client/GlobalUsings.cs
+++ NetCafe/Client/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using NetCafe.Shared.ApiResponses;
 global using NetCafe.Shared.UserRequests;
 global using Blazored.LocalStorage;
+global using NetCafe.Client.Exceptions;
+global using NetCafe.Client.Services;
