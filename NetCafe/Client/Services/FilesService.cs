7fc2d92ff4b9dd62672bdfab4383b096e7ba17f1
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-10-03 12:16:06 +0300

    Implemented the files service

---
+++ NetCafe/Client/Services/FilesService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace NetCafe.Client.Services;
+
+public class FilesService : IFilesService
+{
+    private readonly HttpClient httpClient;
+
+    public FilesService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<ApiResponse<string>> UploadFileAsync(IFormFile file)
+    {
+        var formData = new MultipartFormDataContent();
+        var streamContent = new StreamContent(file.OpenReadStream());
+        formData.Add(streamContent, "file", file.FileName);
+        var response = await httpClient.PostAsync("/api/files", formData);
+        if (!response.IsSuccessStatusCode)
+        {
+            var error = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+            throw new FileUploadFailedException(message: error!.Message!);
+        }
+        var result = await response.Content.ReadFromJsonAsync<ApiResponse<string>>();
+        return result!;
+    }
+}
+
