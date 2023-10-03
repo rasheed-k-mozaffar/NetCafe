099bf6b3645eb82beb103f182cb0b49c5eb2a6a8
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-10-03 12:14:49 +0300

    Created the all posts section on the home page

---
+++ NetCafe/Client/Sections/AllPosts.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NetCafe.Client.Sections;
+
+public partial class AllPosts : ComponentBase
+{
+    [Inject]
+    public NavigationManager Navigation { get; set; } = default!;
+    [Inject]
+    public IPostsService PostsService { get; set; } = default!;
+
+
+    private ApiResponse<IEnumerable<PostSummaryDto>>? requestResult;
+    private List<PostSummaryDto>? posts;
+    private bool isLoadingPosts = true;
+    private string errorMessage = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        isLoadingPosts = true;
+        errorMessage = string.Empty;
+        try
+        {
+            requestResult = await PostsService.GetAllPostsAsync();
+            if (requestResult.IsSuccess)
+            {
+                posts = requestResult.Value?.ToList();
+            }
+        }
+        catch (DataRetrievalFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        isLoadingPosts = false;
+    }
+}
