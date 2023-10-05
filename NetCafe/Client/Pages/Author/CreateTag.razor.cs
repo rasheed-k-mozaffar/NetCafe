5516949dff0ec29f1a734b4fc9b8497ccbfff8d2
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-10-05 15:34:12 +0300

    Created tag management related pages (review)

---
+++ NetCafe/Client/Pages/Author/CreateTag.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NetCafe.Client.Pages.Author;
+
+public partial class CreateTag : ComponentBase
+{
+    [Inject]
+    public NavigationManager Nav { get; set; } = default!;
+    [Inject]
+    public ITagsService TagsService { get; set; } = default!;
+
+    private TagCreateDto tag = new();
+    private bool isMakingRequest = false;
+    private string? errorMessage = string.Empty;
+    private string? successMessage = string.Empty;
+
+    private async Task HandleTagCreationAsync()
+    {
+        isMakingRequest = true;
+        errorMessage = string.Empty;
+        successMessage = string.Empty;
+        try
+        {
+            var result = await TagsService.CreateTagAsync(tag);
+            if (result.IsSuccess)
+            {
+                successMessage = $"New tag ({tag.Name}) was created successfully! You'll be redirected in a moment...";
+                StateHasChanged();
+                await Task.Delay(3000); // wait three seconds before redirecting
+                Nav.NavigateTo("/author/dashboard");
+            }
+        }
+        catch (DataInsertionFailedException ex)
+        {
+            // error message
+            errorMessage = ex.Message;
+        }
+        isMakingRequest = false;
+    }
+}
+
---
+++ NetCafe/Client/Pages/Author/TagsManager.razor.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace NetCafe.Client.Pages.Author;
+
+public partial class TagsManager : ComponentBase
+{
+    [Inject]
+    public ITagsService TagsService { get; set; } = default!;
+
+    private List<TagSummaryDto> tags = new();
+    private bool isLoadingData = true;
+    private string errorMessage = string.Empty;
+
+    // these variable are related to the confirm deletion dialog
+    private bool wantsToDeleteTag = false;
+    private Guid tagToDeleteId;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        // retrieve the tags data
+        errorMessage = string.Empty;
+        try
+        {
+            var results = await TagsService.GetAllTagsAsync();
+            if (results.IsSuccess)
+            {
+                tags = results.Value!.ToList();
+                isLoadingData = false;
+            }
+        }
+        catch (DataRetrievalFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+
+    private async Task DeleteTagAsync()
+    {
+        errorMessage = string.Empty;
+        try
+        {
+            var result = await TagsService.DeleteTagAsync(tagToDeleteId);
+            if (result.IsSuccess)
+            {
+                // remove the tag from the table on the client without
+                // making a request to the server
+                var tagToDelete = tags.FirstOrDefault(t => t.TagId == tagToDeleteId);
+                tags.Remove(tagToDelete!);
+                wantsToDeleteTag = false;
+                StateHasChanged();
+            }
+        }
+        catch (DeletionFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+
+    private void CloseDialog()
+    {
+        tagToDeleteId = default;
+        wantsToDeleteTag = false;
+    }
+
+    private void OpenDeleteDialog(Guid tagId)
+    {
+        wantsToDeleteTag = true;
+        StateHasChanged();
+        tagToDeleteId = tagId;
+    }
+}
