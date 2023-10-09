c4ce6dc1c8138a1812db7240b89b1e4a6d87579c
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-10-09 10:56:44 +0300

    Created the view series related pages

---
+++ NetCafe/Client/Pages/AllSeries.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NetCafe.Client.Pages;
+
+public partial class AllSeries : ComponentBase
+{
+    [Inject]
+    public NavigationManager Nav { get; set; } = default!;
+    [Inject]
+    public ISeriesService SeriesService { get; set; } = default!;
+    private List<SeriesSummaryDto> series = new();
+    private bool isLoadingData = true;
+    private string errorMessage = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        errorMessage = string.Empty;
+        try
+        {
+            var results = await SeriesService.GetAllSeriesAsync();
+            if (results.IsSuccess)
+            {
+                series = results.Value!.ToList();
+                isLoadingData = false;
+            }
+        }
+        catch (DataRetrievalFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+}
