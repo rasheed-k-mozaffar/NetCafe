440ba21a55ea01093ec5ed91cf80da13f7041272
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-10-09 10:57:03 +0300

    Designed and implemented the author pages for the series

---
+++ NetCafe/Client/Pages/Author/CreateSeries.razor.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace NetCafe.Client.Pages.Author;
+
+public partial class CreateSeries : ComponentBase
+{
+    [Inject]
+    public NavigationManager Nav { get; set; } = default!;
+    [Inject]
+    public ISeriesService SeriesService { get; set; } = default!;
+    [Inject]
+    public IFilesService FilesService { get; set; } = default!;
+
+    private bool isMakingRequest = false;
+    private string errorMessage = string.Empty;
+    private string successMessage = string.Empty;
+    private SeriesCreateDto series = new();
+
+    // File upload related properties
+    private string[] allowedFileExtensions = { ".jpeg", ".png", ".jpg" };
+    private int maxCoverImageSize = 1024 * 1024 * 5; // 5MB
+    private string? fileUploadErrorMessage = string.Empty;
+
+    private async Task HandleSeriesCreationAsync()
+    {
+        isMakingRequest = true;
+        errorMessage = string.Empty;
+        successMessage = string.Empty;
+        try
+        {
+            var result = await SeriesService.CreateSeriesAsync(series);
+            if (result.IsSuccess)
+            {
+                successMessage = $"New series ({series.Name}) was created successfully! You'll be redirected in a moment...";
+                StateHasChanged();
+                await Task.Delay(2000);
+                Nav.NavigateTo("/author/dashboard");
+            }
+        }
+        catch (DataInsertionFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        isMakingRequest = false;
+    }
+
+    private async Task UploadCoverImage(InputFileChangeEventArgs e)
+    {
+        if (e.File is null)
+        {
+            fileUploadErrorMessage = "Please select a file!";
+            return;
+        }
+
+        if (e.File.Size > maxCoverImageSize)
+        {
+            fileUploadErrorMessage = "The file you selected has a size that exceeded the max allowed size";
+            return;
+        }
+        isMakingRequest = true;
+        fileUploadErrorMessage = string.Empty;
+        string extension = Path.GetExtension(e.File.Name);
+        // use of an allowed extension
+        if (!allowedFileExtensions.Contains(extension))
+        {
+            fileUploadErrorMessage = $"The image extension ({extension}) is not acceptable";
+            return;
+        }
+        try
+        {
+            // convert IBrowseFile to an IFormFile 
+            var imageFile = await ConvertToIFormFile(e.File);
+            var result = await FilesService.UploadFileAsync(imageFile);
+            // set the cover image URL to the retrieved image URL from the api
+            series.CoverImageUrl = result.Value;
+        }
+        catch (FileUploadFailedException ex)
+        {
+            fileUploadErrorMessage = ex.Message;
+        }
+        isMakingRequest = false;
+    }
+
+    private async Task<Microsoft.AspNetCore.Http.IFormFile> ConvertToIFormFile(IBrowserFile browserFile)
+    {
+        Stream stream = browserFile.OpenReadStream(maxAllowedSize: maxCoverImageSize);
+        MemoryStream memoryStream = new MemoryStream();
+        await stream.CopyToAsync(memoryStream);
+        return new Microsoft.AspNetCore.Http.Internal.FormFile(memoryStream, 0, memoryStream.Length, "file", browserFile.Name);
+    }
+}
---
+++ NetCafe/Client/Pages/Author/SeriesManager.razor.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace NetCafe.Client.Pages.Author;
+
+public partial class SeriesManager : ComponentBase
+{
+    [Inject]
+    public NavigationManager Nav { get; set; } = default!;
+    [Inject]
+    public ISeriesService SeriesService { get; set; } = default!;
+    [Inject]
+    public IFilesService FilesService { get; set; } = default!;
+
+    private List<SeriesSummaryDto> series = new();
+    private bool isLoadingData = true;
+    private string errorMessage = string.Empty;
+
+    // delete series related variables
+    private bool wantsToDeleteSeries = false;
+    private Guid seriesToDeleteId;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        errorMessage = string.Empty;
+        try
+        {
+            var result = await SeriesService.GetAllSeriesAsync();
+            if (result.IsSuccess)
+            {
+                series = result.Value!.ToList();
+                isLoadingData = false;
+            }
+        }
+        catch (DataRetrievalFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+
+    private void OpenDeleteDialog(Guid seriesId)
+    {
+        wantsToDeleteSeries = true;
+        seriesToDeleteId = seriesId;
+    }
+
+    private void CloseDeleteDialog()
+    {
+        wantsToDeleteSeries = false;
+        seriesToDeleteId = default;
+    }
+
+    private async Task DeleteSeriesAsync()
+    {
+        errorMessage = string.Empty;
+        try
+        {
+            var result = await SeriesService.DeleteSeriesAsync(seriesToDeleteId);
+            if (result.IsSuccess)
+            {
+                // delete the series on the page without re loading
+                var seriesToDelete = series.FirstOrDefault(s => s.SeriesId == seriesToDeleteId);
+                series.Remove(seriesToDelete!);
+                wantsToDeleteSeries = false;
+                seriesToDeleteId = default;
+                StateHasChanged();
+            }
+        }
+        catch (DeletionFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+
+
+}
