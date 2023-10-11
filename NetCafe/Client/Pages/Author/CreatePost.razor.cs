b9bb49f48ccd3f3bea99ed4939a08a6b4d419cf1
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-10-11 14:14:50 +0300

    Designed a way to create and view posts

---
+++ NetCafe/Client/Pages/Author/CreatePost.razor.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+
+namespace NetCafe.Client.Pages.Author;
+
+public partial class CreatePost : ComponentBase
+{
+    #region Injected Dependencies
+    [Inject]
+    public NavigationManager Navigation { get; set; } = default!;
+    [Inject]
+    public IFilesService FilesService { get; set; } = default!;
+    [Inject]
+    public IPostsService PostsService { get; set; } = default!;
+    [Inject]
+    public ITagsService TagsService { get; set; } = default!;
+    [Inject]
+    public ISeriesService SeriesService { get; set; } = default!;
+    #endregion
+
+    #region variables
+    private bool isMakingRequest = false;
+    private string errorMessage = string.Empty;
+    private PostCreateDto post = new();
+
+    // Series select related variables
+    private List<SeriesSummaryDto> series = new();
+    private Guid? selectedSeries;
+
+    // Tag select related variables
+    private List<TagSummaryDto> tags = new();
+    IEnumerable<Guid>? selectedTags;
+    private string tagSelectionError = string.Empty;
+
+    // File upload related properties
+    private string[] allowedFileExtensions = { ".jpeg", ".png", ".jpg" };
+    private int maxCoverImageSize = 1024 * 1024 * 5; // 5MB
+    private string? fileUploadErrorMessage = string.Empty;
+    #endregion
+
+    protected override async Task OnInitializedAsync()
+    {
+        errorMessage = string.Empty;
+        await base.OnInitializedAsync();
+        try
+        {
+            var result = await TagsService.GetAllTagsAsync();
+            if (result.IsSuccess)
+            {
+                tags = result.Value!.ToList();
+            }
+
+            var seriesResults = await SeriesService.GetAllSeriesAsync();
+            if (seriesResults.IsSuccess)
+            {
+                series = seriesResults.Value!.ToList();
+            }
+        }
+        catch (DataRetrievalFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+
+    private void TagsChanged()
+    {
+        tagSelectionError = string.Empty;
+        if (selectedTags is not null && selectedTags.Count() > 3)
+        {
+            selectedTags = Enumerable.Empty<Guid>();
+            tagSelectionError = "You can't select more than 3 tags";
+        }
+    }
+    private async Task HandlePostCreation()
+    {
+        isMakingRequest = true;
+        errorMessage = string.Empty;
+        try
+        {
+            if (selectedTags is not null && selectedTags.Any())
+            {
+                post.TagIds = selectedTags.ToArray();
+            }
+
+            if (selectedSeries is not null)
+            {
+                post.SeriesId = selectedSeries;
+            }
+            var result = await PostsService.CreatePostAsync(post);
+            if (result.IsSuccess)
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+        catch (DataInsertionFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+        isMakingRequest = false;
+    }
+
+    #region Cover Image Upload
+    private async Task UploadCoverImage(InputFileChangeEventArgs e)
+    {
+        if (e.File is null)
+        {
+            fileUploadErrorMessage = "Please select a file!";
+            return;
+        }
+
+        if (e.File.Size > maxCoverImageSize)
+        {
+            fileUploadErrorMessage = "The file you selected has a size that exceeded the max allowed size";
+            return;
+        }
+        isMakingRequest = true;
+        fileUploadErrorMessage = string.Empty;
+        string extension = Path.GetExtension(e.File.Name);
+        // use of an allowed extension
+        if (!allowedFileExtensions.Contains(extension))
+        {
+            fileUploadErrorMessage = $"The image extension ({extension}) is not acceptable";
+            return;
+        }
+        try
+        {
+            // convert IBrowseFile to an IFormFile 
+            var imageFile = await ConvertToIFormFile(e.File);
+            string result = await FilesService.UploadFileAsync(imageFile);
+            // set the cover image URL to the retrieved image URL from the api
+            post.CoverImageUrl = result;
+        }
+        catch (FileUploadFailedException ex)
+        {
+            fileUploadErrorMessage = ex.Message;
+        }
+        isMakingRequest = false;
+    }
+    private async Task<Microsoft.AspNetCore.Http.IFormFile> ConvertToIFormFile(IBrowserFile browserFile)
+    {
+        Stream stream = browserFile.OpenReadStream(maxAllowedSize: maxCoverImageSize);
+        MemoryStream memoryStream = new MemoryStream();
+        await stream.CopyToAsync(memoryStream);
+        return new Microsoft.AspNetCore.Http.Internal.FormFile(memoryStream, 0, memoryStream.Length, "file", browserFile.Name);
+    }
+    #endregion
+}
---
+++ NetCafe/Client/Pages/Post.razor.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace NetCafe.Client.Pages;
+
+public partial class Post : ComponentBase
+{
+    [Parameter] public Guid PostId { get; set; }
+
+    [Inject]
+    public NavigationManager Nav { get; set; } = default!;
+    [Inject]
+    public IPostsService PostsService { get; set; } = default!;
+    [Inject]
+    public ISeriesService SeriesService { get; set; } = default!;
+
+    private PostDto post = new();
+    private SeriesDto? series;
+    private bool isLoadingPost = true;
+    private string errorMessage = string.Empty;
+
+    private string postContent = string.Empty;
+
+    private bool isLoadingSeriesData = true;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        errorMessage = string.Empty;
+        try
+        {
+            var result = await PostsService.GetPostByIdAsync(PostId);
+            if (result.IsSuccess)
+            {
+                // after loading the post, convert it into markup so that it can be displayed
+                // in a valid format on the page
+                post = result.Value!;
+                postContent = Markdig.Markdown.ToHtml(post.Content!);
+                isLoadingPost = false;
+            }
+        }
+        catch (DataRetrievalFailedException ex)
+        {
+            errorMessage = ex.Message;
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (post.SeriesId is not null)
+        {
+            try
+            {
+                var seriesRetrievalResult = await SeriesService.GetSeriesByIdAsync((Guid)post.SeriesId);
+                if (seriesRetrievalResult.IsSuccess)
+                {
+                    series = seriesRetrievalResult.Value;
+                    isLoadingSeriesData = false;
+                }
+            }
+            catch (DataRetrievalFailedException ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+    }
+}
